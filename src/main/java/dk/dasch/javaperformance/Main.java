62c09d9c3e21677a39dd3db56a53f4d8b39bc2bc
dasch-dk <daniel@schiermer.dk>
2017-06-27 15:37:03 +0200

    Adding src template

---
+++ src/main/java/dk/dasch/javaperformance/Main.java
@@ -0,0 +1,10 @@
+package dk.dasch.javaperformance;
+
+/**
+ * Created by dasch on 6/25/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
